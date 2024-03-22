2270778c75d9dfc97fe9abe431f2ca9454db0502
Kenneth <kennethyoung.2960@gmail.com>
2024-03-22 01:21:12 -0500

    2 of 3 tests passed

--- Modern_asynchronous_programming_Promise_all_Node_18_1/src/main.js
+++ Modern_asynchronous_programming_Promise_all_Node_18_1/src/main.js
@@ -2,7 +2,22 @@ const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5001";
 
 function bulkDelete(ids) {
-
+    
+    const deleteRequests = ids.map(id => {
+        const url = `${BASE_URL}${id}`;
+        return fetch(url, { method: 'DELETE' })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to delete constellation with ID ${id}`);
+                }
+                return { id };
+            })
+            .catch(error => {
+                console.error(error);
+                return { id };
+            });
+    });
+    return Promise.all(deleteRequests);
 }
 
 module.exports = {
