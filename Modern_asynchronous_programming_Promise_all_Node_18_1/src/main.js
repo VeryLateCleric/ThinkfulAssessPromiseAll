fd9350aa918e3447ec801ca8f0770da703cd4e08
Kenneth <kennethyoung.2960@gmail.com>
2024-03-28 17:41:25 -0500

    All tests passing and code cleaned up

--- Modern_asynchronous_programming_Promise_all_Node_18_1/src/main.js
+++ Modern_asynchronous_programming_Promise_all_Node_18_1/src/main.js
@@ -2,30 +2,10 @@ const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5001/constellations";
 
 
-const ids = ["KGQIwSq", "32TN5F8", "KMS1234"];
-
-// function bulkDelete(ids) {
-//     const deleteRequests = ids.map(id => {
-//         const url = `${BASE_URL}${id}`;
-//         return fetch(url, { method: 'DELETE' })
-//             .then(response => {
-//                 if (!response.ok) {
-//                     throw new Error(`Failed to delete constellation with ID ${id}`);
-//                 }
-//                 return { id: String(id) };
-//             })
-//             .catch(error => {
-//                 console.error(error);
-//                 return { id };
-//             });
-//     });
-//     return Promise.all(deleteRequests);
-// }
-
 async function bulkDelete(ids) {
     try {
         const deleteRequests = ids.map(id => {
-            const url = `${BASE_URL}${id}`;
+            const url = `${BASE_URL}/${id}`;
             console.log(`Request is made to ${url}`)
             return axios.delete(url).then(() => ({id}));
         })
@@ -36,11 +16,9 @@ async function bulkDelete(ids) {
         console.log('Return value:', deletedItems);
         return deletedItems
     } catch (error) {
-        console.error('Error')
+        console.error('Error during bulk deletion', error)
     }
 }
-
-bulkDelete(ids).then(console.log);
 
 module.exports = {
   bulkDelete,
