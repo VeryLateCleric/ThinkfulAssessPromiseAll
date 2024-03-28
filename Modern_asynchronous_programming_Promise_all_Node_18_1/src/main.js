81575967e77769c05ef6c6a562e3ffc353cfbcb3
Kenneth <kennethyoung.2960@gmail.com>
2024-03-28 17:15:29 -0500

    Rewritten using async and try{}catch{} lessons

--- Modern_asynchronous_programming_Promise_all_Node_18_1/src/main.js
+++ Modern_asynchronous_programming_Promise_all_Node_18_1/src/main.js
@@ -1,25 +1,45 @@
 const axios = require("../utils/axios");
 const BASE_URL = "http://localhost:5001/constellations";
 
-function bulkDelete(ids) {
-    const deleteRequests = ids.map(id => {
-        const url = `${BASE_URL}${id}`;
-        return fetch(url, { method: 'DELETE' })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Failed to delete constellation with ID ${id}`);
-                }
-                return { id };
-            })
-            .catch(error => {
-                console.error(error);
-                return { id };
-            });
-    });
-    return Promise.all(deleteRequests);
-}
 
 const ids = ["KGQIwSq", "32TN5F8", "KMS1234"];
+
+// function bulkDelete(ids) {
+//     const deleteRequests = ids.map(id => {
+//         const url = `${BASE_URL}${id}`;
+//         return fetch(url, { method: 'DELETE' })
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error(`Failed to delete constellation with ID ${id}`);
+//                 }
+//                 return { id: String(id) };
+//             })
+//             .catch(error => {
+//                 console.error(error);
+//                 return { id };
+//             });
+//     });
+//     return Promise.all(deleteRequests);
+// }
+
+async function bulkDelete(ids) {
+    try {
+        const deleteRequests = ids.map(id => {
+            const url = `${BASE_URL}${id}`;
+            console.log(`Request is made to ${url}`)
+            return axios.delete(url).then(() => ({id}));
+        })
+
+        const results = await Promise.all(deleteRequests)
+        
+        const deletedItems = results.map(result => ({ id: result.id }));
+        console.log('Return value:', deletedItems);
+        return deletedItems
+    } catch (error) {
+        console.error('Error')
+    }
+}
+
 bulkDelete(ids).then(console.log);
 
 module.exports = {
